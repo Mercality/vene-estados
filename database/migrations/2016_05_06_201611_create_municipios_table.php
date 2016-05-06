9162f7ec71c0a4c060ae56d63bd84ff1516c1fdd
Mercality <ramonlv93@gmail.com>
2016-05-06 16:49:09 -0400

    Initial Commit

---
+++ database/migrations/2016_05_06_201611_create_municipios_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMunicipiosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('municipios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('estado_id')->unsigned()->index();
+            $table->timestamps();
+
+            $table->foreign('estado_id')->references('id')->on('estados')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('municipios');
+    }
+}
---
+++ database/migrations/2016_05_06_201618_create_ciudades_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCiudadesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ciudades', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('estado_id')->unsigned()->index();
+            $table->boolean('capital');
+            $table->timestamps();
+
+            $table->foreign('estado_id')->references('id')->on('estados');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('ciudades');
+    }
+}
