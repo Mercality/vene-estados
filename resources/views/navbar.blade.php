b309f0b4cc0124cba583fd3d2ec3c63ce2ead04d
Mercality <ramonlv93@gmail.com>
2016-05-25 21:14:41 -0400

    more content and navbar tweaks

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,26 +1,24 @@
 <nav class="blue darken-2">
-    <div class="nav-wrapper">
-    
+    <div class="nav-wrapper container">
+
     <a id="menu-btn" class="btn btn-flat hide-on-med-and-up">
         <i class="material-icons">menu</i>
     </a>
-    
+
       <a href="#" class="brand-logo">VeneDatos</a>
       <ul id="nav-mobile" class="right hide-on-small-only">
         <li><a href="sass.html">VenEstados</a></li>
         <li><a href="badges.html">VenRIF</a></li>
         <li><a href="collapsible.html">Ven</a></li>
       </ul>
-      
+
       <ul id="slide-out" class="side-nav">
         <li><a href="sass.html">VenEstados</a></li>
         <li><a href="badges.html">VenRIF</a></li>
         <li><a href="collapsible.html">Ven</a></li>
       </ul>
-      
-      
+
+
     </div>
-    <a href="#" data-activates="slide-out" class="button-collapse"><i class="mdi-navigation-menu"></i></a>  
+    <a href="#" data-activates="slide-out" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
 </nav>
-      
-        
